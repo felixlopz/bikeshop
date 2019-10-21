77998e3e4a57d8cd097628067bd3b7ce3ec4abd0
felix <felixcodesg@gmail.com>
2019-10-20 20:00:34 -0400

    finishin components section

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 const navToggler = document.querySelector(".navigation__toggler");
 const navIcon = document.querySelector(".navigation__icon");
+const navList = document.querySelector(".navigation__list");
 const navItems = document.querySelectorAll(".navigation__item");
 
 navToggler.addEventListener('click',  () => {
   navIcon.classList.toggle("navigation__icon--open");
+  navList.classList.toggle("navigation__list--open");
   navItems.forEach( (item , i) => {
     setTimeout ( () => {
       item.classList.toggle("navigation__item--close");
